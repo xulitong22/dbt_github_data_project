6a3e0174bca590ce191c0809ddf086e2794419a3
xulitong22 <annexlt@yahoo.com>
2026-04-20 15:23:13 +0200

    staging layer created

---
+++ dbt_github/models/staging/stg_issues.sql
@@ -0,0 +1,24 @@
+with source as (
+    select * from {{source('github_raw', 'issues')}}
+),
+
+renamed as (
+    select
+        id as issue_id,
+        number as issue_number,
+        state,
+        title,
+        user.login as author_username,
+        user.id as author_id,
+        labels, 
+        cast(created_at as timestamp) as created_at,
+        cast(updated_at as timestamp) as updated_at,
+        cast(closed_at as timestamp) as closed_at,
+        body
+    from source
+    where pull_request is null
+)
+
+select
+*
+from renamed
---
+++ dbt_github/models/staging/stg_pull_requests.sql
@@ -0,0 +1,22 @@
+with source as (
+    select * from {{source('github_raw', 'pull_requests')}}
+),
+
+renamed as (
+    select
+        id as pr_id,
+        number as pr_number,
+        state,
+        title,
+        user.login as author_username,
+        user.id as author_id,
+        cast(created_at as timestamp) as created_at,
+        cast(updated_at as timestamp) as updated_at,
+        cast(closed_at as timestamp) as closed_at,
+        cast(merged_at as timestamp) as merged_at,
+        labels,
+        body
+    from source
+)
+
+select * from renamed
