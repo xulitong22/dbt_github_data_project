bb43e86274bb49c4c5e7b56a7feb7b759f8c3c62
xulitong22 <annexlt@yahoo.com>
2026-04-20 22:31:30 +0200

    add model labels to intermidiate layer

---
+++ dbt_github/models/intermediate/int_github_labels.sql
@@ -0,0 +1,25 @@
+with item_with_labels as(
+    select
+    issue_id as item_id,
+    'issue' as item_type,
+    labels
+    from {{ref('stg_issues')}}
+
+    union all 
+
+    select
+    pr_id as item_id,
+    'pull request' as item_type,
+    labels
+    from {{ref('stg_pull_requests')}}
+)
+
+select
+item_id,
+item_type,
+label.id as label_id,
+label.name as label_name,
+label.color as label_color,
+label.description as label_description
+from item_with_labels
+cross join unnest(labels) as label
